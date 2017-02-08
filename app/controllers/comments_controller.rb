39ebbf626e58a153d619c314cef3918bf2c5ec44
awanderlyss <alyssa.felix7@gmail.com>
2017-02-08 03:18:19 -0500

    Form for post complete, form for comment has method error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,12 +24,14 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to post_path(@post), notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: post_path(@post) }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
