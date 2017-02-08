8e7cfe66203639aedd05d8394cad2149a80117b1
awanderlyss <alyssa.felix7@gmail.com>
2017-02-08 15:48:25 -0500

    Remove unness files in views/comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @post = Post.find(params[:post_id])
-    @comments = @post.comments.order(:created_at)
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-  end
-
   # GET /comments/new
   def new
     @post = Post.find(params[:post_id])
