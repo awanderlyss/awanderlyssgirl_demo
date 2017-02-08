353e0080bc84b04dfd3066177427455f958349be
awanderlyss <alyssa.felix7@gmail.com>
2017-02-08 17:59:53 -0500

    Remove all files ass. to ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  mount Ckeditor::Engine => '/ckeditor'
-
+  
   devise_for :users
 
   resources :posts do
