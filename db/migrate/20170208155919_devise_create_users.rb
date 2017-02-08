3a4df34c59b1ca1198b4e2313dd7709ef30a8ca5
awanderlyss <alyssa.felix7@gmail.com>
2017-02-08 11:02:42 -0500

    Add admin boolean to user model

--- db/migrate/20170208155919_devise_create_users.rb
+++ db/migrate/20170208155919_devise_create_users.rb
@@ -30,8 +30,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
-      t.string :message
+      t.string  :name
+      t.string  :message
+      t.boolean :admin
 
       t.timestamps null: false
     end
