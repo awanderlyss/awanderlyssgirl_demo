fdb0b78b64b9c7cf2c9d57d3e62d61a706cab93c
awanderlyss <alyssa.felix7@gmail.com>
2017-02-09 21:54:28 -0500

    Refactor comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,35 @@
-
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:new, :create, :show, :edit, :update, :destroy]
+
   def new
-    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params.merge(user: current_user))
     redirect_to post_path(@post)
   end
-  def edit
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-  end
+
   def update
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
     @comment.update(comment_params.merge(user:current_user))
     redirect_to post_path(@post)
   end
+
   def destroy
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
+  
   private
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
     def comment_params
       params.require(:comment).permit(:body)
     end
