824d96613e6d679216a2b2d62bdfabb862e6cedc
awanderlyss <alyssa.felix7@gmail.com>
2017-02-09 21:51:19 -0500

    Refactor post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,36 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
-  def show
-    @post = Post.find(params[:id])
-  end
-  def edit
-    @post = Post.find(params[:id])
-  end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.create!(post_params.merge(user: current_user))
     redirect_to post_path(@post)
   end
+
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params.merge(user:current_user))
     redirect_to post_path(@post)
   end
+
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
+
   private
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+    def set_post
+      @post = Post.find(params[:id])
+    end
+    
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
