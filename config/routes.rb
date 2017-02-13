eb263c3120bbfe62bb834f809428628b6070acee
awanderlyss <alyssa.felix7@gmail.com>
2017-02-12 22:49:30 -0500

    Mount actioncable to /cable url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount ActionCable.server => '/cable'
 
   resources :chat_rooms, only: [:new, :create, :show, :index]
 
